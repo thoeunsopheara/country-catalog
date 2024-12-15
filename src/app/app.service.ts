f4b3450d69084b30176b8f75ebfcd9e791d0f5b1
sopheara-kdc <sopheara@kirirom-digital.com>
2024-12-15 21:06:24 +0700

    implement app service

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { CountryResponse } from './types/response';
+import { Observable } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class AppService {
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  getAllCountries(search?: string): Observable<CountryResponse[]> {
+    if (!search) return this.http.get<CountryResponse[]>('https://restcountries.com/v3.1/all');
+    return this.http.get<CountryResponse[]>(`https://restcountries.com/v3.1/name/${search}`);
+  }
 }
